0bdf12408daa23110d4d6e64dffaeec2fe81ec87
ulisesd404 <144632800+ulisesd404@users.noreply.github.com>
2024-04-11 22:27:19 -0700

    models added

---
+++ utils/auth.js
@@ -0,0 +1,13 @@
+// Purpose: Middleware to check if the user is logged in before allowing them to access certain routes
+
+const withAuth = (req, res, next) => {
+    // If the user is not logged in, redirect the request to the login route
+    if (!req.session.logged_in) {
+      res.redirect('/login');
+    } else {
+      next();
+    }
+  };
+  
+  module.exports = withAuth;
+  
